c7c32bf6cfbe3c96ca2c60b48bcdddd2076078b4
invays <ayan.paul1234@yandex.ru>
2020-06-23 12:52:50 +0300

    Revert "Add new version for Opencart/Ocstore 3.0.2"
    This reverts commit 5cdb532dfffa3c85e839edfc3b94c423c22a8e64.

--- Opencart3/upload/admin/language/ru-ru/extension/module/images_finder.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_['heading_title']     = 'Images finder';
-$_['text_extension']    = 'Модули';
-$_['text_success']      = 'Внимание! Вы изменили настройки модуля!';
-$_['text_edit']         = 'Редактировать модуль';
-$_['entry_status']      = 'Статус';
-$_['text_agree']        = '';
-$_['text_autor']        = 'Made by invays: <a href="https://opencartmodul.ru" target"_blank"> visit autor webpage</a>';
-$_['error_permission']  = 'Внимание! У вас нет прав на изменение настроек модуля!';
